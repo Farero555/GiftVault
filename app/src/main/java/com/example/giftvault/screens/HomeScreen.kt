d1c686ca88c1c0feeff7d04d0fa10f894b0358b8
Arturs Karavackis <karavackis2@gmail.com>
2024-05-12 18:12:43 +0300

    modify HomeScreen

--- app/src/main/java/com/example/giftvault/screens/HomeScreen.kt
+++ app/src/main/java/com/example/giftvault/screens/HomeScreen.kt
@@ -1,17 +1,84 @@
 package com.example.giftvault.screens
 
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.giftvault.ui.theme.Ecru
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(navController: NavController){
-    TopAppBar(
-        title = {
-            Text("GiftVault")
+fun HomeScreen(navController: NavController) {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    fun toggleDrawer() {
+        coroutineScope.launch {
+            if (drawerState.isClosed) {
+                drawerState.open()
+            } else {
+                drawerState.close()
+            }
         }
-    )
+    }
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            DrawerContent()
+        }
+    ) {
+        Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            topBar = {
+                CenterAlignedTopAppBar(
+                    modifier = Modifier.height(42.dp),
+                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                        containerColor = Ecru,
+                        titleContentColor = Color.White
+                    ),
+                    title = { Text("GiftVault") },
+                    navigationIcon = {
+                        IconButton(onClick = { toggleDrawer() }) {
+                            Icon(
+                                imageVector = Icons.Filled.Menu,
+                                contentDescription = "Menu"
+                            )
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = { /* Handle other actions here */ }) {
+                            Icon(
+                                imageVector = Icons.Filled.AccountCircle,
+                                contentDescription = "Profile"
+                            )
+                        }
+                    }
+                )
+            }
+        ) {
+            Column(modifier = Modifier.padding(it)) {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun DrawerContent() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text("Drawer Item 1")
+        Text("Drawer Item 2")
+    }
 }
